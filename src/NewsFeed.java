21b2be15e472aba8269602ddda6fb68e0d25963c
Vivaan Malhotra <malhot56@purdue.edu>
2024-11-02 18:20:39 -0400

    Update NewsFeed.java

--- src/NewsFeed.java
+++ src/NewsFeed.java
@@ -1,27 +1,13 @@
 import java.util.*;
 public class NewsFeed {
-    private ArrayList<Post> posts;
-
-    public void createPost(String content, Picture picture) {
-        Post post = new Post(content, picture);
-        post.setId(posts.size() + 1);
-        posts.add(post);
-        System.out.println("Post created with ID: " + post.getId());
-    }
-
+    private List<Post> posts;
     // Deleting a Post
-    public void deletePost(int postId) throws PostException {
-        boolean removed = posts.removeIf(post -> post.getId() == postId);
-        if (!removed) {
-            throw new PostException("Post with ID " + postId + " not found.");
-        }
-        System.out.println("Post with ID " + postId + " deleted.");
-    }
+   
     public void displayPosts(User user) {
-        ArrayList<User> friends = user.getFriends();
+        List<User> friends = user.getFriends();
         for (int i = 0; i < friends.size(); i++) {
             User friend = friends.get(i);
-            ArrayList<Post> friendPosts= friend.getPosts();
+            List<User> friendPosts = friend.getPosts();
             for (int j = 0; j < friendPosts.size(); j++) {
                 posts.add(friendPosts.get(j));
             }
@@ -31,13 +17,4 @@ public class NewsFeed {
             System.out.println(posts.get(k));
         }
     }
-    public void upvote(Post post) {
-        int votes = post.getVotes();
-        votes++;
-    }
-    public void downvote(Post post) {
-        int votes = post.getVotes();
-        votes--;
-    }
-
 }
