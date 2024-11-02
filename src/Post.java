d9a98cac8cfaac99009ed2d6f58d15d72c5a26be
Vivaan Malhotra <malhot56@purdue.edu>
2024-11-02 18:43:37 -0400

    Update Post.java

--- src/Post.java
+++ src/Post.java
@@ -48,11 +48,65 @@ public class Post implements IPost {
         System.out.println("Post downvoted. Total downvotes: " + totalDownVotes);
     }
 
-
-
+    @Override
+    public void addComment(String comment) {
+        if (!commentsEnabled) {
+            System.out.println("Comments are disabled for this post.");
+            return;
+        }
+        int commentId = comments.size() + 1;
+        comments.put(commentId, comment);
+        System.out.println("Comment added with ID: " + commentId);
+    }
 
     @Override
+    public void deleteComment(int commentId) {
+        if (comments.remove(commentId) != null) {
+            System.out.println("Comment with ID " + commentId + " deleted.");
+        } else {
+            System.out.println("Comment with ID " + commentId + " not found.");
+        }
+    }
 
+    @Override
+    public void hidePost() {
+        this.hidden = true;
+        System.out.println("Post is now hidden.");
+    }
+
+    @Override
+    public void deletePost(User user) {
+        // Ensure the post is removed from the user's list of posts
+        if (user.getPosts().remove(this)) {
+            System.out.println("Post with ID " + this.id + " removed from user's posts.");
+        } else {
+            System.out.println("Post with ID " + this.id + " was not found in user's posts.");
+        }
+
+        // Clear comments and reset upvotes and downvotes
+        comments.clear();
+        totalUpVotes -= this.upVotes;
+        totalDownVotes -= this.downVotes;
+        this.upVotes = 0;
+        this.downVotes = 0;
+        System.out.println("All comments cleared, and upvotes/downvotes reset for post ID: " + this.id);
+
+        // Set hidden status for post, marking it as inactive (optional)
+        this.hidden = true;
+        System.out.println("Post with ID " + this.id + " is now hidden and deleted.");
+    }
+
+    @Override
+    public void enableComments() {
+        this.commentsEnabled = true;
+        System.out.println("Comments have been enabled for this post.");
+    }
+
+    @Override
+    public void disableComments() {
+        this.commentsEnabled = false;
+        System.out.println("Comments have been disabled for this post.");
+    }
     public User getAuthor() {
         return this.author;
     }
@@ -112,3 +166,4 @@ public class Picture {
         return url;
     }
 }
+
