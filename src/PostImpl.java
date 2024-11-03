2a2c426600343e8f1b8e05e3b024c5fc96babd1c
vaishisharma <sharm911@purdue.edu>
2024-11-03 01:11:31 -0500

    Create PostImpl.java
    Post Implementation class created to account for abstract Post class

---
+++ src/PostImpl.java
@@ -0,0 +1,12 @@
+package src;
+
+public class PostImpl extends Post {
+    public PostImpl(String content, User user) {
+        super(content, user);
+    }
+
+    @Override
+    public void deletePost() {
+
+    }
+}
