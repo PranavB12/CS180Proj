66323f0858b8a6d19f9c5a5fd9a52d4f7f80d6cf
Pranav Bansal <pranavbansal1214@gmail.com>
2024-11-03 16:32:42 -0500

    Update AuthenticationException.java

--- src/AuthenticationException.java
+++ src/AuthenticationException.java
@@ -1,3 +1,5 @@
+package src;
+
 public class AuthenticationException extends RuntimeException implements IAuthenticationException {
     public AuthenticationException(String message) {
         super(message);
