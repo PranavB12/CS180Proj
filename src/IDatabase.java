31c9408da4002f55c4d58126b7627e8fa18a9af7
RishiRao42 <raosrishi@gmail.com>
2024-11-03 15:14:58 -0500

    fixed database and stuff

--- src/IDatabase.java
+++ src/IDatabase.java
@@ -3,42 +3,23 @@ package src;
 public interface IDatabase {
 
     boolean addUser(User user);
-
     boolean removeUser(User user);
-
     boolean createAccount(String username, String password, String name);
-
     boolean validateCredentials(String username, String password);
-
     boolean userExists(String username);
-
     int createPost(String content, User author);
-
     boolean deletePost(int postId);
-
     User viewUser(String username);
-
     boolean addFriend(User user, User friend);
-
     boolean removeFriend(User user, User friend);
-
     boolean blockUser(User user, User toBlock);
-
     void upvotePost(int postId);
-
     void downvotePost(int postId);
-
     void addCommentToPost(int postId, String comment);
-
     void deleteCommentFromPost(int postId, int commentId);
-
     void hidePost(int postId);
-
     void enableCommentsForPost(int postId);
-
     void disableCommentsForPost(int postId);
-
     void saveDatabaseToFile(String filename);
-
     void readDatabaseFromFile(String filename);
 }
