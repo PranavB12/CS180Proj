ae4f8341aa09095d88d630bba5580bc0b267fc95
Vivaan Malhotra <malhot56@purdue.edu>
2024-11-15 14:29:44 -0500

    Create Comment.java

---
+++ src/Comment.java
@@ -0,0 +1,53 @@
+import java.util.UUID;
+
+public class Comment {
+    private String comment;
+    private String ID;
+    private User author;
+    private int upVotes;
+    private int downVotes;
+    private String postID;
+
+    public Comment(String comment, User author, String postID) {
+        this.comment = comment;
+        String Id = UUID.randomUUID().toString();
+        this.ID = Id;
+        this.upVotes = 0;
+        this.downVotes = 0;
+        this.postID = postID;
+        this.author = author;
+
+
+    }
+
+
+
+    public int getDownVotes() {
+        return downVotes;
+    }
+
+    public int getUpVotes() {
+        return upVotes;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+    public String getAuthor() {
+        return this.author.toString();
+    }
+    public void upVote() {
+        this.upVotes ++;
+    }
+    public void downVote() {
+        this.downVotes ++;
+    }
+    public String getID() {
+        return ID;
+    }
+    public String toString() {
+        return "COMMENT|" + this.ID + "|" + this.author.getUsername() + "|" + this.comment;
+    }
+
+
+}
