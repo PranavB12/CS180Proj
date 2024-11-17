1d56c4dcfed73e9d1ac48197c033f574b90ae243
Pranav Bansal <pranavbansal1214@gmail.com>
2024-11-16 21:21:45 -0500

    Update IComment.java

--- src/IComment.java
+++ src/IComment.java
@@ -1,14 +1,27 @@
 package src;
 
-public interface IComment {
-    String getComment();
-    String getAuthor();
-    String getID();
-    int getUpVotes();
-    int getDownVotes();
+/**
+ * Group Project - CS18000 Gold
+ *
+ * Interface for the comment class
+ *
+ * @author Pranav Bansal, Vivaan Malhotra, Rishi Rao, Mike Lee, lab sec 37
+ *
+ * @version November 16, 2024
+ *
+ */
 
+public interface IComment {
+
+    String getPostID();
+    int getDownVotes();
+    int getUpVotes();
+    String getContent();
+    User getAuthor();
     void upVote();
     void downVote();
-
+    void undoUpVote();
+    void undoDownVote();
+    String getID();
     String toString();
 }
