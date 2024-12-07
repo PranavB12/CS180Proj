ee877dc232b10161080f4b705855743b82c0275c
Pranav Bansal <pranavbansal1214@gmail.com>
2024-12-06 22:28:26 -0500

    Update INewsFeed.java

--- src/INewsFeed.java
+++ src/INewsFeed.java
@@ -3,6 +3,16 @@ package src;
 
 import java.util.*;
 
+/**
+ * Group Project - CS18000 Gold
+ *
+ * Newsfeed class interface
+ *
+ * @author Pranav Bansal, Vivaan Malhotra, Rishi Rao, Mike Lee lab sec 37
+ *
+ * @version November 19, 2024
+ *
+ */
 
 public interface INewsFeed {
     void displayPosts(User user);
