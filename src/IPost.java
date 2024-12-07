71a5b1ae360d7b0aefc770d8af2569e9bddde8e6
Pranav Bansal <pranavbansal1214@gmail.com>
2024-12-06 22:28:41 -0500

    Update IPost.java

--- src/IPost.java
+++ src/IPost.java
@@ -3,6 +3,16 @@ package src;
 
 import java.util.*;
 
+/**
+ * Group Project - CS18000 Gold
+ *
+ * Post class interface
+ *
+ * @author Pranav Bansal, Vivaan Malhotra, Rishi Rao, Mike Lee lab sec 37
+ *
+ * @version November 19, 2024
+ *
+ */
 
 public interface IPost {
     String getId();
